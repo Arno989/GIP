e2c487385f322deff0d36256b728716735ce94b4
Arno Defauw <arno@delandtsheer.eu>
2018-03-23 10:42:09 +0100

    Small stuff

--- Presentation/Site/ClientPage.aspx.designer.cs
+++ Presentation/Site/ClientPage.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace Presentation.Site {
         protected global::System.Web.UI.WebControls.LinkButton btnEdit;
         
         /// <summary>
-        /// UpdatePanel1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
-        
-        /// <summary>
         /// GridView control.
         /// </summary>
         /// <remarks>
@@ -47,14 +38,5 @@ namespace Presentation.Site {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView;
-        
-        /// <summary>
-        /// ScriptManager1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager1;
     }
 }
