f0bf8ad2df35b5ebc0a37779c1bf64237d0d629b
Cédric De Meester <cedricje.demeester@gmail.com>
2018-04-19 20:16:40 +0200

    bug fix

--- Presentation/Site/DoctorPage.aspx.cs
+++ Presentation/Site/DoctorPage.aspx.cs
@@ -112,13 +112,6 @@ namespace Presentation.Site
             Response.Redirect("../Site/DoctorPage.aspx");
         }
 
-        public static string YesModClick(object sender, EventArgs e)
-        {
-            string strToRtn = "";
-            // The code that you want to execute when the user clicked yes goes here
-            return strToRtn;
-        }
-
         protected void Add(object sender, EventArgs e)
         {
             Session["DataID"] = null;
