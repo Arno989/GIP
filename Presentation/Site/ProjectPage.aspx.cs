47865e9f48844a948295ad5b2548dd73387f2df3
Arno Defauw <arno@delandtsheer.eu>
2018-06-03 19:58:46 +0200

    mini fix

--- Presentation/Site/ProjectPage.aspx.cs
+++ Presentation/Site/ProjectPage.aspx.cs
@@ -34,7 +34,7 @@ namespace Presentation.Site
             {
                 var container = Master.FindControl("Body");
                 string lbName = "lbRel1";
-                ListBox listbox = GridView.Rows[i].Cells[5].FindControl(lbName) as ListBox;
+                ListBox listbox = GridView.Rows[i].Cells[4].FindControl(lbName) as ListBox;
                 List<int> Relations = _businesscode.GetRelationProjectHasCRAs(Convert.ToInt32(GridView.DataKeys[i].Value)); //--Var
 
                 if (Relations.Count != 0)
@@ -58,7 +58,7 @@ namespace Presentation.Site
             {
                 var container = Master.FindControl("Body");
                 string lbName = "lbRel2";
-                ListBox listbox = GridView.Rows[i].Cells[6].FindControl(lbName) as ListBox;
+                ListBox listbox = GridView.Rows[i].Cells[5].FindControl(lbName) as ListBox;
                 List<int> Relations = _businesscode.GetRelationProjectHasDoctors(Convert.ToInt32(GridView.DataKeys[i].Value)); //--Var
 
                 if (Relations.Count != 0)
@@ -82,7 +82,7 @@ namespace Presentation.Site
             {
                 var container = Master.FindControl("Body");
                 string lbName = "lbRel3";
-                ListBox listbox = GridView.Rows[i].Cells[7].FindControl(lbName) as ListBox;
+                ListBox listbox = GridView.Rows[i].Cells[6].FindControl(lbName) as ListBox;
                 List<int> Relations = _businesscode.GetRelationProjectHasHospitals(Convert.ToInt32(GridView.DataKeys[i].Value)); //--Var
 
                 if (Relations.Count != 0)
@@ -106,7 +106,7 @@ namespace Presentation.Site
             {
                 var container = Master.FindControl("Body");
                 string lbName = "lbRel4";
-                ListBox listbox = GridView.Rows[i].Cells[8].FindControl(lbName) as ListBox;
+                ListBox listbox = GridView.Rows[i].Cells[7].FindControl(lbName) as ListBox;
                 List<int> Relations = _businesscode.GetRelationProjectHasProjectManagers(Convert.ToInt32(GridView.DataKeys[i].Value)); //--Var
 
                 if (Relations.Count != 0)
